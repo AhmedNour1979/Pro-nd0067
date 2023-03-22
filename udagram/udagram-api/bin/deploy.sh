a90b14ffece6079e27357dc5afaf8c85b736786b
Ahmed Nour <ahmed.nour.1979@gmail.com>
2023-03-22 15:53:18 +0200

    update backend deploy.sh file

--- udagram/udagram-api/bin/deploy.sh
+++ udagram/udagram-api/bin/deploy.sh
@@ -5,20 +5,20 @@ echo 'finished eb init'
 eb list
 eb use Pro-nd0067-dev
 echo 'now using Pro-nd0067-dev'
-eb setenv AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
-eb setenv AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION
-eb setenv AWS_SECRT_ACCESS_KEY=$AWS_SECRT_ACCESS_KEY
-eb setenv POSTGRES_DB=$POSTGRES_DB
-eb setenv POSTGRES_HOST=$POSTGRES_HOST
-eb setenv POSTGRES_Port=$POSTGRES_Port
-eb setenv POSTGRES_USERNAME=$POSTGRES_USERNAME
-eb setenv POSTGRES_PASSWORD=$POSTGRES_PASSWORD
-eb setenv AWS_REGION=$AWS_REGION
-eb setenv AWS_PROFILE=$AWS_PROFILE
-eb setenv AWS_BUCKET=$AWS_BUCKET
-eb setenv URL=$URL
-eb setenv ServerPort=$ServerPort
-eb setenv JWT_SECRET=$JWT_SECRET
+eb setenv AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID --timeout 1400
+eb setenv AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION --timeout 1400
+eb setenv AWS_SECRT_ACCESS_KEY=$AWS_SECRT_ACCESS_KEY --timeout 1400
+eb setenv POSTGRES_DB=$POSTGRES_DB --timeout 1400
+eb setenv POSTGRES_HOST=$POSTGRES_HOST --timeout 1400
+eb setenv POSTGRES_Port=$POSTGRES_Port --timeout 1400
+eb setenv POSTGRES_USERNAME=$POSTGRES_USERNAME --timeout 1400
+eb setenv POSTGRES_PASSWORD=$POSTGRES_PASSWORD --timeout 1400
+eb setenv AWS_REGION=$AWS_REGION --timeout 1400
+eb setenv AWS_PROFILE=$AWS_PROFILE --timeout 1400
+eb setenv AWS_BUCKET=$AWS_BUCKET --timeout 1400
+eb setenv URL=$URL --timeout 1400
+eb setenv ServerPort=$ServerPort --timeout 1400
+eb setenv JWT_SECRET=$JWT_SECRET --timeout 1400
 echo 'finished setup enviroment variables'
 cp -rf src/config www/config
 cp -R .elasticbeanstalk www/.elasticbeanstalk
@@ -29,7 +29,6 @@ zip -r Archive.zip .
 cd ..
 echo 'finished coping files and Ziping'
 eb list
-eb use Pro-nd0067-dev
 echo 'now will start eb deploy'
 eb deploy
 echo 'finished eb deploy'
